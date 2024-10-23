f30140baf40f0f9cf4c6ea27f32b9097b2dfb2af
thsergitox <sergiopezoj@gmail.com>
2024-10-23 17:21:29 -0500

    feat: minor improvments

--- src/api/gmail/connection.ts
+++ src/api/gmail/connection.ts
@@ -1,6 +1,6 @@
-import { google } from 'googleapis'
-import { getRefreshToken, saveNewRefreshToken } from '~/database'
-import dotenv from 'dotenv'
+import dotenv from 'dotenv';
+import { google } from 'googleapis';
+import { saveNewRefreshToken } from '~/database';
 
 dotenv.config();
 
@@ -10,7 +10,7 @@ if (!process.env.CLIENT_ID || !process.env.CLIENT_SECRET || !process.env.REDIREC
 
 const initializeOAuthClient = async () => {
     try {
-        const REFRESH_TOKEN = await getRefreshToken()
+        const REFRESH_TOKEN = process.env.REFRESH_TOKEN
 
         if (!REFRESH_TOKEN) {
             throw new Error('No refresh token found.')
@@ -22,19 +22,64 @@ const initializeOAuthClient = async () => {
             process.env.REDIRECT_URIS
         );
 
-        oAuth2Client.setCredentials({
-            refresh_token: REFRESH_TOKEN,
+        // generate a url that asks permissions for Blogger and Google Calendar scopes
+        const scopes = [
+           'https://mail.google.com/'
+        ];
+        
+        const url = oAuth2Client.generateAuthUrl({
+            // 'online' (default) or 'offline' (gets refresh_token)
+            access_type: 'offline',
+        
+            // If you only need one scope, you can pass it as a string
+            scope: scopes
         });
+        
+        try {
+           
+            oAuth2Client.setCredentials({
+                refresh_token: REFRESH_TOKEN,
+            });
+    
+            oAuth2Client.on('tokens', (tokens) => {
+                if (tokens.refresh_token) {
+                    console.log('New refresh token received');
+                    saveNewRefreshToken(tokens.refresh_token);
+                }
+                console.log('New access token received');
+            });
 
-        oAuth2Client.on('tokens', (tokens) => {
-            if (tokens.refresh_token) {
-                console.log('New refresh token')
-                saveNewRefreshToken(tokens.refresh_token)
+
+            
+
+
+    
+           
+            // Forzar una renovación de tokens
+            try {
+                await oAuth2Client.getAccessToken();
+                console.log('Successfully refreshed access token');
+            } catch (error) {
+                console.error('Error refreshing access token:', error);
+                if ((error as any).response && (error as any).response.data.error === 'invalid_grant') {
+                    console.error('The refresh token is invalid or has been revoked. You need to re-authenticate.');
+                    // Aquí podrías implementar lógica para reiniciar el proceso de autenticación
+                    throw new Error('Authentication required');
+                }
+                throw error;
             }
-            console.log('New access token:', tokens.access_token)
-        });
+    
+            console.log('OAuth2 client initialized');
+            return oAuth2Client;
+        } catch (e) {
+            console.error('Error setting credentials:', e);
+            throw e;
+        }
 
-        return oAuth2Client;
+
+        
+
+        
 
     } catch (error) {
         console.error('Error initializing OAuth2 client:', error);
--- src/jobs/gmail/spamEmails.ts
+++ src/jobs/gmail/spamEmails.ts
@@ -1,15 +1,24 @@
 import cron from 'node-cron'
 import { addMailSpamToBlacklist } from '~/services'
 
-cron.schedule('0 0 */14 * *', async ()  => { 
-
+// Ejecutar el job inmediatamente al inicio
+(async () => {
    try {
-    await addMailSpamToBlacklist()
-    console.log('MailSpam Job executed')
+      await addMailSpamToBlacklist()
+      console.log('MailSpam Job executed at startup')
    } catch (error) {
-    console.log('There was an error', error)
+      console.log('There was an error at startup', error)
    }
-    
+})()
+
+// Programar el job para que se ejecute cada 14 días
+cron.schedule('0 0 */14 * *', async ()  => { 
+   try {
+      await addMailSpamToBlacklist()
+      console.log('MailSpam Job executed')
+   } catch (error) {
+      console.log('There was an error', error)
+   }
 })
 
 export default cron
