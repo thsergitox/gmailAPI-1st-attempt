5c97ae1abe99598a21df693ced9b3461e004abd6
thsergitox <sergiopezoj@gmail.com>
2024-10-23 19:02:37 -0500

    refactor: update prompt for spam verification

--- src/api/groq/connection.ts
+++ src/api/groq/connection.ts
@@ -7,11 +7,13 @@ const client = new Groq({
   apiKey: process.env.GROQ_API_KEY, 
 });
 
+const PROMPT = '"Determine whether the following email is spam, phishing, or illegitimate. If it is, respond only with 1. If it is not spam, respond only with 0. Do not include any explanations or additional text. Note: We are an association of computer science students and are not interested in purchasing any products or services."'
+
 export const verifySpamWithAI = async (emailsInfo: Array<{id: string, body: string, spam?: boolean}>) => {
   const promises = emailsInfo.map(async (emailInfo) => {
     const chatCompletion = await client.chat.completions.create({
       messages: [
-        { role: 'system', content: '"Determine whether the following email is spam, phishing, or illegitimate. If it is, respond only with 1. If it is not spam, respond only with 0. Do not include any explanations or additional text. Note: We are an association of computer science students and are not interested in purchasing any products or services."' },
+        { role: 'system', content:  PROMPT},
         { role: 'user', content: `Email: ${emailInfo.body}` },
       ],
       model: 'llama3-8b-8192',
